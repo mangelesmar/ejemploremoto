f855ccb301018e3e892acbaf3947e4630b7d881d
mamartinezf <mamartinezf@indra.es>
2017-11-24 11:56:17 +0100

    Prueba para Jenkins

--- src/main/java/com/mycompany/ejemploremoto/Principal.java
+++ src/main/java/com/mycompany/ejemploremoto/Principal.java
@@ -17,6 +17,7 @@ public class Principal {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Inicio");
+        System.out.println("Inicio2");
     }
     
 }
